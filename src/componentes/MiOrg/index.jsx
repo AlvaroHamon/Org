de03a8b038b777685f3e72e130c678ebfacc17ff
AlvaroHamon <alvaro.hamon@gmail.com>
2023-09-01 02:55:43 -0500

    se actualizaron lineas que no se estaban usando para finalizar

--- src/componentes/MiOrg/index.jsx
+++ src/componentes/MiOrg/index.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
 import "./MiOrg.css";
 
 const MiOrg = (props) => {
-  //Estado - hooks
-  //useState
-  //const [nombreVariable, funcionActulizar] = useState(valor inicial)
 
-  // const [mostrar, actualizarMostrar] = useState(true);
-  // const manejarClick = () => {
-  //   console.log("Mostrar/Ocultar elemento");
-  //   actualizarMostrar(!mostrar);
-  // };
   return (
     <section className="orgSection">
       <h3 className="tittle">Mi Organización</h3>
