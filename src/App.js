60b14069e26d7a2014a47f34d0d56cbe09f8dcf5
AlvaroHamon <alvaro.hamon@gmail.com>
2023-08-31 23:44:32 -0500

    agregando funcionalidades para cambiar el color y alistando funcion para eliminar persona del equipo

--- src/App.js
+++ src/App.js
@@ -12,15 +12,9 @@ function App() {
 
     {
       equipo: "Front End",
-      foto: "https://github.com/alvarohamon.png",
-      nombre: "Alvaro Hamon",
-      puesto: "Desarrollador Front End"
-    },
-    {
-      equipo: "Front End",
       foto: "https://github.com/harlandlohora.png",
       nombre: "Harland Lohora",
-      puesto: "Instructor en Alura Latam"
+      puesto: "Instructor"
     },
     {
       equipo: "Programación",
@@ -29,7 +23,7 @@ function App() {
       puesto: "Desarrolladora de software e instructora"
     },
     {
-      equipo: "Data Science",
+      equipo: "UX y Diseño",
       foto: "https://github.com/JeanmarieAluraLatam.png",
       nombre: "Jeanmarie Quijada",
       puesto: "Instructora en Alura Latam"
@@ -38,27 +32,16 @@ function App() {
       equipo: "Programación",
       foto: "https://github.com/christianpva.png",
       nombre: "Christian Velasco",
-      puesto: "Head de Alura e instructor"
+      puesto: "Head de Alura e Instructor"
     },
     {
-      equipo: "Data Science",
+      equipo: "Innovación y Gestión",
       foto: "https://github.com/JoseDarioGonzalezCha.png",
       nombre: "Jose Gonzalez",
-      puesto: "Dev. FullStack"
+      puesto: "Dev FullStack"
     }
   ])
-
-  const cambiarMostrar = () => {
-    actualizarMostrar(!mostrarFormulario)
-  }
-
-  const registrarColaborador = (colaborador) => {
-    // console.log("Nuevo Colaborador", colaborador)
-    actualizarColaboradores([...colaboradores, colaborador])
-  };
-
-  //Lista de equipos
-  const equipos = [
+  const [equipos, actualiarEquipos] = useState([
     {
       titulo: "Programación",
       colorPrimario: "#57C278",
@@ -100,7 +83,77 @@ function App() {
       colorPrimario: "#FF8A29",
       colorSecundario: "#FFEEDF"
     }
-  ];
+  ])
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  const registrarColaborador = (colaborador) => {
+    // console.log("Nuevo Colaborador", colaborador)
+    actualizarColaboradores([...colaboradores, colaborador])
+  };
+
+  //Eliminar Colaborador
+  const eliminarColaborador = () => {
+    console.log("boton eliminar");
+  }
+
+  //Actualizar color de equipo
+
+  const actualizarColor = (color, titulo) => {
+    const equiposActualizados = equipos.map((equipo) => {
+      if (equipo.titulo === titulo) {
+        equipo.colorPrimario = color
+      }
+      return equipo
+    })
+    actualiarEquipos(equiposActualizados)
+  }
+  //Lista de equipos
+  // const equipos = [
+  //   {
+  //     titulo: "Programación",
+  //     colorPrimario: "#57C278",
+  //     colorSecundario: "#D9F7E9",
+  //   },
+
+  //   {
+  //     titulo: "Front End",
+  //     colorPrimario: "#82CFFA",
+  //     colorSecundario: "#E8F8FF"
+  //   },
+
+  //   {
+  //     titulo: "Data Science",
+  //     colorPrimario: "#A6D157",
+  //     colorSecundario: "#F0F8E2"
+  //   },
+
+  //   {
+  //     titulo: "Dev Ops",
+  //     colorPrimario: "#E06B69",
+  //     colorSecundario: "#FDE7E8"
+  //   },
+
+  //   {
+  //     titulo: "UX y Diseño",
+  //     colorPrimario: "#DB6EBF",
+  //     colorSecundario: "#FAE9F5"
+  //   },
+
+  //   {
+  //     titulo: "Movil",
+  //     colorPrimario: "#FFBA05",
+  //     colorSecundario: "#FFF5D9"
+  //   },
+
+  //   {
+  //     titulo: "Innovación y Gestión",
+  //     colorPrimario: "#FF8A29",
+  //     colorSecundario: "#FFEEDF"
+  //   }
+  // ];
 
   return (
     <div>
@@ -117,6 +170,8 @@ function App() {
           datos={equipo}
           key={equipo.titulo}
           colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
         />
         )
       }
--- src/componentes/Equipo/index.jsx
+++ src/componentes/Equipo/index.jsx
@@ -1,23 +1,37 @@
+import hexToRgba from "hex-to-rgba";
 import Colaborador from "../Colaborador";
 import "./Equipo.css";
 
 const Equipo = (props) => {
   //props para el estilo CSS
   const { colorPrimario, colorSecundario, titulo } = props.datos;
-  const secundario = { backgroundColor: colorSecundario };
-  const principal = { borderColor: colorPrimario };
+  const secundario = { backgroundColor: hexToRgba(colorPrimario, "0.6") };
+  const estiloTexto = { borderColor: colorPrimario };
 
   //Lista de colaboradores
-  const { colaboradores } = props;
+  const { colaboradores, eliminarColaborador, actualizarColor } = props;
 
   return (
     <>
       {colaboradores.length > 0 && (
         <section className="equipo" style={secundario}>
-          <h3 style={principal}>{titulo}</h3>
+          <input
+            type="color"
+            className="input-color"
+            value={hexToRgba(colorPrimario, "0.6")}
+            onChange={(evento) => {
+              actualizarColor(evento.target.value, titulo);
+            }}
+          />
+          <h3 style={estiloTexto}>{titulo}</h3>
           <div className="colaboradores">
             {colaboradores.map((colaborador, index) => (
-              <Colaborador datos={colaborador} key={index} colorPrimario={colorPrimario}/>
+              <Colaborador
+                datos={colaborador}
+                key={index}
+                colorPrimario={colorPrimario}
+                eliminarColaborador={eliminarColaborador}
+              />
             ))}
           </div>
         </section>
